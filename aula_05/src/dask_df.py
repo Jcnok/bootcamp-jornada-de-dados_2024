896a0c309209a056bfa00fdae48e07c08452ed84
Julio Okuda <julio.okuda@gmail.com>
2024-03-23 00:31:42 -0300

    add script: dask.py

---
+++ aula_05/src/dask_df.py
@@ -0,0 +1,17 @@
+# Dask => script para caluclar min, max e mean em um bilhão de linhas.
+from utils.decorators import timer_to_csv
+import dask
+dask.config.set({'dataframe.query-planning': True})
+import dask.dataframe as dd
+@timer_to_csv
+def dask_df():    
+    # Ler o arquivo txt diretamente em um DataFrame Dask
+    df = dd.read_csv('data/measurements.txt', delimiter=';', 
+                     header=None, names=['city', 'temp'])
+    # min, max, e mean pela cidade ordenado pelo index
+    return print(df.groupby('city').
+                   agg({'temp': ['max','min','mean']}).
+                   compute().round(2).sort_index())
+
+if __name__ == "__main__":
+    dask_df()
