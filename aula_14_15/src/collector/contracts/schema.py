8ae8a557342cf0e116f61a26c5b54e40f92470d1
Julio Okuda <julio.okuda@gmail.com>
2024-04-30 16:38:41 -0300

    update: all codes

---
+++ aula_14_15/src/collector/contracts/schema.py
@@ -0,0 +1,12 @@
+# Schemma
+from typing import Dict, Union
+
+GenericSchema = Dict[str, Union[str, float, int]]
+
+
+CompraSchema: GenericSchema = {
+    "ean": int,
+    "price": float,
+    "store": int,
+    "dateTime": str,
+}
