33b17c4bb88bc95c2b78a4325a0bd2a82166dc89
Julio Okuda <julio.okuda@gmail.com>
2024-05-14 01:04:55 -0300

    update: scripts ok

--- aula_19/src/main.py
+++ aula_19/src/main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-import database
+import data
 import models
 from fastapi import Depends, FastAPI, HTTPException
 from schema import Item, ItemCreate
@@ -8,11 +8,11 @@ from sqlalchemy.orm import Session
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=database.engine)
+models.Base.metadata.create_all(bind=data.engine)
 
 
 @app.post("/items/", response_model=Item)
-def create_item(item: ItemCreate, db: Session = Depends(database.get_db)):
+def create_item(item: ItemCreate, db: Session = Depends(data.get_db)):
     db_item = models.Item(**item.dict())
     db.add(db_item)
     db.commit()
@@ -21,13 +21,13 @@ def create_item(item: ItemCreate, db: Session = Depends(database.get_db)):
 
 
 @app.get("/items/", response_model=List[Item])
-def read_items(skip: int = 0, limit: int = 10, db: Session = Depends(database.get_db)):
+def read_items(skip: int = 0, limit: int = 10, db: Session = Depends(data.get_db)):
     items = db.query(models.Item).offset(skip).limit(limit).all()
     return items
 
 
 @app.get("/items/{item_id}", response_model=Item)
-def read_item(item_id: int, db: Session = Depends(database.get_db)):
+def read_item(item_id: int, db: Session = Depends(data.get_db)):
     db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
@@ -35,7 +35,7 @@ def read_item(item_id: int, db: Session = Depends(database.get_db)):
 
 
 @app.put("/items/{item_id}", response_model=Item)
-def update_item(item_id: int, item: ItemCreate, db: Session = Depends(database.get_db)):
+def update_item(item_id: int, item: ItemCreate, db: Session = Depends(data.get_db)):
     db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
@@ -47,7 +47,7 @@ def update_item(item_id: int, item: ItemCreate, db: Session = Depends(database.g
 
 
 @app.delete("/items/{item_id}", response_model=Item)
-def delete_item(item_id: int, db: Session = Depends(database.get_db)):
+def delete_item(item_id: int, db: Session = Depends(data.get_db)):
     db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
