5f457b06bc6417cf16c05f37f16f61f3da536c8c
Julio Okuda <julio.okuda@gmail.com>
2024-06-03 23:29:52 -0300

    add: exercício_02

---
+++ bootcamp-SQL-e-analytics/aula_05/SQL/exercicio_02.sql
@@ -0,0 +1,39 @@
+-- 02.Faça uma análise de crescimento mensal e o cálculo de YTD
+-- Certifique-se de que há índices adequados nas colunas usadas para junções e filtros
+CREATE INDEX IF NOT EXISTS idx_orders_order_date ON orders(order_date);
+CREATE INDEX IF NOT EXISTS idx_order_details_order_id ON order_details(order_id);
+
+WITH ReceitasMensais AS (
+    SELECT
+        EXTRACT(YEAR FROM orders.order_date) AS Ano,
+        EXTRACT(MONTH FROM orders.order_date) AS Mes,
+        SUM(order_details.unit_price * order_details.quantity * (1.0 - order_details.discount)) AS Receita_Mensal
+    FROM
+        orders
+    INNER JOIN
+        order_details ON orders.order_id = order_details.order_id
+    GROUP BY
+        EXTRACT(YEAR FROM orders.order_date),
+        EXTRACT(MONTH FROM orders.order_date)
+),
+ReceitasAcumuladas AS (
+    SELECT
+        Ano,
+        Mes,
+        Receita_Mensal,
+        SUM(Receita_Mensal) OVER (PARTITION BY Ano ORDER BY Mes) AS Receita_YTD
+    FROM
+        ReceitasMensais
+)
+SELECT
+    Ano,
+    Mes,
+    Receita_Mensal,
+    Receita_Mensal - LAG(Receita_Mensal) OVER (PARTITION BY Ano ORDER BY Mes) AS Diferenca_Mensal,
+    Receita_YTD,
+	(Receita_Mensal - LAG(Receita_Mensal) OVER (PARTITION BY Ano ORDER BY Mes))
+	/ LAG(Receita_Mensal) OVER (PARTITION BY Ano ORDER BY Mes) * 100 AS Percentual_Mudanca_Mensal
+FROM
+    ReceitasAcumuladas
+ORDER BY
+    Ano, Mes;
